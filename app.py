14848730b479277ebc648469a2100fc12bd96b30
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-12-04 03:27:00 +0530

    Add "Frontend" given by Pankhuri

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import time
 import os
 from flask import Flask, render_template, request, session, escape
 import random
+import csv
+import urllib
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'WHY_SO_MUCH_TORTURE?')
@@ -14,7 +16,15 @@ app.secret_key = os.getenv('SECRET_KEY', 'WHY_SO_MUCH_TORTURE?')
 
 @app.route('/')
 def home_page():
-    return render_template('home.html', greeting='Hello From Group 11. Our EVS Project on climate change')
+    p = [['Country', 'Value']]
+    country_list_link = 'https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/world-country-names.tsv'
+    f = urllib.request.urlopen(country_list_link)
+    countries = [[row.split('\t')[1], random.randint(30, 50)] for row in f.read().decode('UTF-8').split('\n') if row]
+    # print(countries)
+    countries.pop(0)
+    p.extend(countries)
+    years = [2016, 2017, 2018]
+    return render_template('evs.html', years=years, countries=p)
 
 @app.route('/<country>/<year>')
 def get_prediction(country, year):
@@ -23,4 +33,4 @@ def get_prediction(country, year):
 port = int(os.getenv('VCAP_APP_PORT', 8080))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
